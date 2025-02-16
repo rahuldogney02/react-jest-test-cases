83b96df756417bf15d071843d1dbbc3300bcf553
rahuldogney02 <rahuldogney02@gmail.com>
2025-02-16 12:36:10 +0530

    remove unused code

--- jest-app/src/__tests__/UserFunctional.js
+++ jest-app/src/__tests__/UserFunctional.js
@@ -2,7 +2,7 @@
 import handleOtherData from "../helper";
 import UserFunction from "../UserFunctional";
 import { fireEvent, render, screen } from '@testing-library/react';
-import handleOtherData from "../helper";
+
 test("Method testing with events",()=>{
     render(<UserFunction/>);
     const btn = screen.getByTestId("btn1");
