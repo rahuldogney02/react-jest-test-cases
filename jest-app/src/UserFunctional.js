c3ac7146ff4c20cf28bde014c51ab5de9f2b9083
rahuldogney02 <rahuldogney02@gmail.com>
2025-02-16 12:35:32 +0530

    functional testing Video-16

--- jest-app/src/UserFunctional.js
+++ jest-app/src/UserFunctional.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import handleOtherData from './helper';
 
 function UserFunction(){
     
@@ -9,11 +9,16 @@ function UserFunction(){
         setData(val);
     }
 
+    // const handleOtherData = () =>{
+    //     console.log("hi")
+    //     return "hi"
+    // } This function is exported from other file for test-cases
+
     return(
         <>
         <h1>Hello {data}</h1>
         <button data-testid='btn1' onClick={handleData}>Click me</button> {/* testing of method using event */}
-
+        <button  onClick={handleOtherData}>Click me for other data</button> 
         </>
     );
     
--- jest-app/src/__tests__/UserFunctional.js
+++ jest-app/src/__tests__/UserFunctional.js
@@ -1,10 +1,15 @@
 
+import handleOtherData from "../helper";
 import UserFunction from "../UserFunctional";
 import { fireEvent, render, screen } from '@testing-library/react';
-
+import handleOtherData from "../helper";
 test("Method testing with events",()=>{
     render(<UserFunction/>);
     const btn = screen.getByTestId("btn1");
     fireEvent.click(btn);
     expect(screen.getByText("Hello")).toBeInTheDocument();
+})
+
+test("method test case2",()=>{
+    expect(handleOtherData()).toMatch("hi")
 })
